98c27db9bd04f6a74cf54a4d0258b7db6c558967
shivasishsaksham86-hub <shivasishsaksham86@gmail.com>
2026-07-07 02:32:57 +0530

    d

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { prisma } from '../server';
 import { generateAccessToken, generateRefreshToken } from '../utils/jwt';
 
